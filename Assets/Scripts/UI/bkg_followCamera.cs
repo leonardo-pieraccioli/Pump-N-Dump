bb298231786576c17699324d1af48dd7dd5e1d9d
MartinaPlumari <martinaplumari@hotmail.it>
2025-01-25 16:56:21 +0100

    [ADD] grid fucking logic

---
+++ Assets/Scripts/UI/bkg_followCamera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class bkg_followCamera : MonoBehaviour
+{
+    [SerializeField] private Transform t_camera;
+    public float snap;
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        Vector3 pos = new Vector3(Mathf.Round(t_camera.position.x/snap)*snap,Mathf.Round(t_camera.position.y/snap)*snap, 0f);
+        transform.position = pos;
+    }
+}
